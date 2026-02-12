b483160d1879714ee2e15b19b6d726dcc612b819
teiling88 <teiling88@gmail.com>
2026-02-12 14:44:52 +0100

    NTR: add caching for rss_forum.php

--- htdocs/rss_forum.php
+++ htdocs/rss_forum.php
@@ -1,6 +1,16 @@
 <?php
+$cache_file = __DIR__ . '/var/cache2/staticmap/rss_forum_cache.xml';
+$cache_lifetime = 600;
 $atom_url = 'https://forum.opencaching.de/app.php/feed';
-$atom_xml = @simplexml_load_file($atom_url);
+
+if (file_exists($cache_file) && (time() - filemtime($cache_file) < $cache_lifetime)) {
+    $atom_xml = @simplexml_load_file($cache_file);
+} else {
+    $atom_xml = @simplexml_load_file($atom_url);
+    if ($atom_xml) {
+        @file_put_contents($cache_file, $atom_xml->asXML());
+    }
+}
 
 if (!$atom_xml) {
     header('HTTP/1.1 500 Internal Server Error');
@@ -9,8 +19,7 @@ if (!$atom_xml) {
 
 header('Content-Type: application/rss+xml; charset=utf-8');
 
-echo '<?xml version="1.0" encoding="UTF-8" ?>' . PHP_EOL;
-?>
+echo '<?xml version="1.0" encoding="UTF-8" ?>' . PHP_EOL;?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
         <title><?php echo htmlspecialchars($atom_xml->title); ?> (RSS Proxy)</title>
@@ -26,7 +35,7 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>' . PHP_EOL;
                 <guid isPermaLink="false"><?php echo htmlspecialchars($entry->id); ?></guid>
                 <pubDate><?php echo date(DATE_RSS, strtotime($entry->updated)); ?></pubDate>
                 <description>
-                    <![CDATA[<?php echo $entry->content; ?>]]>
+                    <![CDATA[<?php echo htmlspecialchars($entry->content); ?>]]>
                 </description>
             </item>
         <?php endforeach; ?>
